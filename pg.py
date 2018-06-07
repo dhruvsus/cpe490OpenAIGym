b348a9661504d8c3d5520a8e746f4dcef133f59e
Thomas Fahrner <33201730+tfahrn@users.noreply.github.com>
2018-06-07 10:01:20 -0700

    policy gradient cartpole

---
+++ pg.py
@@ -0,0 +1,106 @@
+import gym
+import numpy as np
+from keras.models import Model
+from keras import layers
+from keras import backend as K
+from keras import utils as np_utils
+from keras import optimizers
+
+
+def build_network(num_inputs, num_outputs):
+    # use Keras functional API to construct policy network
+    network = layers.Input(shape=(num_inputs,))
+    input = network
+
+    for layer in [32, 16, 8]:
+        network = layers.Dense(layer)(network)
+        network = layers.Activation("relu")(network)
+
+    network = layers.Dense(num_outputs)(network)
+    network = layers.Activation("softmax")(network)
+
+    return Model(inputs=input, outputs=network)
+
+
+def build_train(model, num_outputs):
+    prob_action = model.output
+    encoded_action = K.placeholder(shape=(None, num_outputs), name="encoded_action")
+    discount_reward = K.placeholder(shape=(None,), name="discount_reward")
+
+    loss = K.mean(-K.log(K.sum(prob_action * encoded_action, axis=1)) * discount_reward)
+
+    deltas = optimizers.Adam().get_updates(params=model.trainable_weights, loss=loss)
+
+    return K.function(inputs=[model.input, encoded_action, discount_reward], outputs=[], updates=deltas)
+
+
+def get_action(model, state, num_outputs):
+    state = np.expand_dims(state, axis=0)
+    prob_action = np.squeeze(model.predict(state))
+
+    return np.random.choice(np.arange(num_outputs), p=prob_action)
+
+
+def fit(train, states, actions, rewards, num_outputs):
+    encoded_action = np_utils.to_categorical(actions, num_classes=num_outputs)
+    discount_reward = get_discounted(rewards)
+    train([states, encoded_action, discount_reward])
+
+
+def get_discounted(rewards):
+    discount_rate = 0.96
+    discounted = np.zeros_like(rewards, dtype=np.float32)
+    sum = 0
+
+    for t in reversed(range(len(rewards))):
+        sum = sum * discount_rate + rewards[t]
+        discounted[t] = sum 
+
+    discounted -= discounted.mean() / discounted.std()
+
+    return discounted
+
+
+def run_episode(env, train, model, num_outputs):
+    is_done = False
+    state = env.reset()
+    states = []
+    actions = []
+    rewards = []
+    total_reward = 0
+
+    while not is_done:
+        action = get_action(model, state, num_outputs)
+        new_state, reward, is_done, info = env.step(action)
+        total_reward += reward
+
+        states.append(state)
+        actions.append(action)
+        rewards.append(reward)
+
+        state = new_state
+
+    fit(train, np.asarray(states), np.asarray(actions), np.asarray(rewards), num_outputs)
+
+    return total_reward
+
+
+def main():
+    env = gym.make("CartPole-v1")
+    num_inputs = env.observation_space.shape[0]
+    num_outputs = env.action_space.n
+    model = build_network(num_inputs, num_outputs)
+    train = build_train(model, num_outputs)
+
+    for episode in range(1000):
+        reward = run_episode(env, train, model, num_outputs)
+        print(episode, reward)
+
+    
+
+
+    env.close()
+
+
+if __name__ == '__main__':
+    main()
